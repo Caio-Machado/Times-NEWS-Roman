54fba3f405eb622e4f14cb4dd7b632031c8482f6
FlutLab.io <>
2022-06-05 21:26:24 +0000

    Autosave

--- lib/carousel_tile.dart
+++ lib/carousel_tile.dart
@@ -13,7 +13,7 @@ class CarouselTile extends StatelessWidget {
               padding: EdgeInsets.only(top: 15, bottom: 15),
               color: Colors.black,
               child: Row(children: [
-                Center(Text('Título Categoria', style: TextStyle(fontSize: 25, color: Colors.white)))
+                Center(child: Text('Título Categoria', style: TextStyle(fontSize: 25, color: Colors.white)))
               ]))
         ]));
   }
