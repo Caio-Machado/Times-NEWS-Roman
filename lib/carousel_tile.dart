1f4fd44862a5c21150938752726f7d65084c85c0
FlutLab.io <>
2022-06-05 20:18:12 +0000

    Autosave

--- lib/carousel_tile.dart
+++ lib/carousel_tile.dart
@@ -8,7 +8,7 @@ class CarouselTile extends StatelessWidget {
         margin: EdgeInsets.only(top: 50, bottom: 90),
         decoration: BoxDecoration(image: DecorationImage(image: NetworkImage('https://substackcdn.com/image/fetch/w_1200,h_600,c_limit,f_jpg,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fccb48d1f-2a82-45be-996e-a34b6126af34_1280x1280.png'), fit: BoxFit.cover)),
         child: Column(children: [
-          Text('Título Categoria', style: TextStyle(fontSize: 35, backgroundColor: Colors.black, color: Colors.white))
+          Container(margin: EdgeInsets.only(top: 45), child: Text('Título Categoria', style: TextStyle(fontSize: 35, backgroundColor: Colors.black, color: Colors.white)))
         ]));
   }
 }
