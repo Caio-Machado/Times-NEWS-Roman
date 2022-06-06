973f81c0a9364e3ab9ffb283b937a4eb21d4e009
FlutLab.io <>
2022-06-06 00:34:05 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,6 @@ import 'carousel.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child: SingleChildScrollView(child: Carousel()));
+    return Container(child: Carousel());
   }
 }
