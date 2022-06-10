a0ce17ace9ed7d1d7e4f0d81dc35dfa928b92386
FlutLab.io <>
2022-06-10 01:34:21 +0000

    Autosave

--- lib/carousel_tile.dart
+++ lib/carousel_tile.dart
@@ -9,7 +9,7 @@ class CarouselTile extends StatelessWidget {
         margin: const EdgeInsets.all(7),
         decoration: BoxDecoration(boxShadow: [
           BoxShadow(color: Colors.black.withOpacity(1), spreadRadius: 5, blurRadius: 7, offset: const Offset(0, 3))
-        ], borderRadius: const BorderRadius.all(Radius.circular(15)), image: DecorationImage(colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.65), BlendMode.darken), image: NetworkImage('https://substackcdn.com/image/fetch/w_1200,h_600,c_limit,f_jpg,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fccb48d1f-2a82-45be-996e-a34b6126af34_1280x1280.png'), fit: BoxFit.cover)),
+        ], borderRadius: const BorderRadius.all(Radius.circular(15)), image: DecorationImage(colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.65), BlendMode.darken), image: NetworkImage('https://s.abcnews.com/images/US/maryland-shooting-ht-ps-220609_1654806584945_hpMain_16x9_1600.jpg'), fit: BoxFit.cover)),
         child: SingleChildScrollView(
             child: Column(children: [
           Header(title: 'Título Categoria'),
