3e59c86172d98f876575e5f604e052cd5ec5c352
FlutLab.io <>
2022-06-10 02:51:43 +0000

    Autosave

--- lib/home/carousel.dart
+++ lib/home/carousel.dart
@@ -11,7 +11,12 @@ class Carousel extends StatelessWidget {
             child: PageView.builder(
                 itemCount: 7,
                 itemBuilder: (_, currentIndex) {
-                  return CarouselTile();
+                  return CarouselTile(
+                    categoryTitle: 'Título categoria',
+                    newsTitle: 'Título Notícia',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    image: 'https://static.politico.com/15/49/549fc7b34126a8195fe9352a58f5/https-delivery.gettyimages.com/downloads/1237661675',
+                  );
                 }))
       ],
     ));
