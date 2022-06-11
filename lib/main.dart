efebce774dce22cda8c72dab96a9000f1a2576ce
FlutLab.io <>
2022-06-11 02:56:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'templates/scaffold.dart';
 import 'home/home.dart';
+import 'list_news/category_news.dart';
 
 void main() => runApp(MainWidget());
 
@@ -11,10 +12,12 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/': (context) => Home(),
+        '/list': (context) => CategoryNews(),
+      },
       theme: ThemeData(appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF161314))),
-      home: ScaffoldTemplate(
-        content: Home(),
-      ),
+      initialRoute: '/',
     );
   }
 }
