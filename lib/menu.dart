29708fdb764795f9d528b9e1e4a52a3fd2a74a87
FlutLab.io <>
2022-06-14 01:05:41 +0000

    Autosave

--- lib/menu.dart
+++ lib/menu.dart
@@ -21,57 +21,20 @@ class Menu extends StatelessWidget {
               title: Text('Início', style: TextStyle(fontSize: 20, color: Colors.white)),
             )),
         InkWell(
-            onTap: () {
-              changeScreen(context, '/list');
-            },
-            child: const Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                child: ListTile(
-                  leading: Icon(Icons.attach_money, size: 27, color: Colors.white),
-                  title: Text('Negócios', style: TextStyle(fontSize: 15, color: Colors.white)),
-                ))),
-        const Padding(
+          onTap: () {
+            changeScreen(context, '/list');
+          },
+          child: const Padding(
             padding: EdgeInsets.only(top: 10, bottom: 10),
             child: ListTile(
-              leading: Icon(Icons.tv, size: 27, color: Colors.white),
-              title: Text('Entreterimento', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.article, size: 27, color: Colors.white),
-              title: Text('Geral', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.health_and_safety, size: 27, color: Colors.white),
-              title: Text('Saúde', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.science, size: 27, color: Colors.white),
-              title: Text('Ciência', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.sports_soccer, size: 27, color: Colors.white),
-              title: Text('Esportes', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.computer, size: 27, color: Colors.white),
-              title: Text('Tecnologia', style: TextStyle(fontSize: 15, color: Colors.white)),
-            )),
-        const Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: ListTile(
-              leading: Icon(Icons.search, size: 27, color: Colors.white),
-              title: Text('Pesquisar', style: TextStyle(fontSize: 15, color: Colors.white)),
-            ))
+              leading: Icon(Icons.attach_money, size: 27, color: Colors.white),
+              title: Text(
+                'Negócios',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
